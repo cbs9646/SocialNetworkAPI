54980a76b5e3c176ca5dd1562c8a30c6052fb5f7
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2022-01-18 21:04:47 -0600

    added thought route coding, tied to thought controller

---
+++ routes/thoughtRoute.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const reactionRoute = require("./reactionRoute");
+
+router.use('/:thoughtId/reactions', reactionRoute);
+
+const {
+    getAllThoughts,
+    getThoughtWithId,
+    newThought,
+    updateThoughtWithId,
+    deleteThoughtWithId,
+} = require("../controllers/thoughtController");
+
+router.route("/thoughts").get(getAllThoughts).post(newThought);
+
+router 
+    .route("thoughts/:thoughtId")
+    .get(getThoughtWithId)
+    .delete(deleteThoughtWithId)
+    .put(updateThoughtWithId);
+
+module.exports = router;
