d0308b38738aa1bf85b2436c34967c457d11a897
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2022-01-18 20:34:02 -0600

    added userroute code

---
+++ routes/userRoute.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const friendRoute = require("./friendRoute");
+
+router.use('/:userId/friends', friendRoute);
+
+const{
+    getAllUsers,
+    getUsersById,
+    createNewUser,
+    updateUserWithId,
+    removeUserById,
+} = require('../controllers/userController');
+
+router.route("/").get(getAllUsers).post(createNewUser);
+
+router
+    .route("/:userId")
+    .get(getUsersById)
+    .delete(removeUserById)
+    .put(updateUserWithId);
+
+    module.exports = router;
